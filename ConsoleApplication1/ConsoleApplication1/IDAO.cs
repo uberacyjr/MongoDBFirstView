43242608644eb1ad59488385d630da24021eb15e
uberacy <uberacyjr@gmail.com>
2017-01-29 20:38:55 -0200

    Alterações

---
+++ ConsoleApplication1/ConsoleApplication1/IDAO.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    public interface IDAO
+    {
+        void Complete();
+    }
+    public interface IPessoa
+    {
+        void AlteraNome();
+        void Complete();
+    }
+    public interface IDocumentos
+    {
+        void AlteraDocumento();
+        void Complete();
+    }
+
+    public class PessoaRepo : IPessoa
+    {
+        private IDocumentos _doc;
+        private string Contexto;
+        public PessoaRepo(IDocumentos _doc)
+        {
+            Contexto = "Novo Contexto";
+            this._doc = _doc;
+        }
+        public void AlteraNome()
+        {
+            //logica para alterar nome pessoa
+            _doc.AlteraDocumento();
+            Complete();
+        }
+
+        public void Complete()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -1,7 +1,9 @@
 ﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
 using MongoDB.Driver;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,10 +16,26 @@ namespace MongoDBFirstLook
         {
             var client = new MongoClient();
             var db = client.GetDatabase("Teste");
+
             var coll = db.GetCollection<Pessoa>("Pessoa");
-            var pessoas = coll.Find<Pessoa>(x=>true).ToList();
+            var collc = db.GetCollection<Carro>("Carros");
+
+            
+            var pessoas = coll.Find(x=>true).ToList();
+            
+
         }
-        public class Pessoa
+        public class Pessoa 
+        {
+            public ObjectId _id { get; set; }
+            public string Nome { get; set; }
+
+            public ObjectId Carros { get; set; }
+
+            public Carro carro { get; set; }
+
+        }
+        public class Carro
         {
             public ObjectId _id { get; set; }
             public string Nome { get; set; }
